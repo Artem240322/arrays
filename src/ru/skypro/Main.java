05711f96202faa0cdd73f61b52a8fae12570b940
ArtemCH240322 <sun2383@mail.ru>
2022-04-13 15:41:18 +0300

    Первый комит. Теория. ДЗ 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,33 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] weights = new int[] {0, 1, 2, 3};
+        for (int i = 0; i < weights.length - 1; i++){
+            System.out.println(weights[i] + " ,");
+        }
+        System.out.println(weights[weights.length - 1]);
+System.out.println();
+//
+    double[] weightsFractional = new double[] {1.57, 7.654, 9.986};
+    for (int u = 0; u<weightsFractional.length-1; u++) {
+        System.out.println(weightsFractional[u]+" ,");
     }
-}
+    System.out.println(weightsFractional[weightsFractional.length-1]);
+
+
+        weights[0] = 12;
+        int januaryWeights = weights[0];
+        System.out.println(januaryWeights);
+        System.out.println(weights[0]);
+        System.out.println(weights[3]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
+            
+
+        }
+    }
+
