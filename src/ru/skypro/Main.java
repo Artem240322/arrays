61130b149e267f1ed2fdda382c2fa41075365083
ArtemCH240322 <sun2383@mail.ru>
2022-04-14 11:17:29 +0300

    Первый комит. Теория. Задачи //1,2,3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -9,27 +9,21 @@ public class Main {
         }
         System.out.println(weights[weights.length - 1]);
 System.out.println();
-//
+//2
     double[] weightsFractional = new double[] {1.57, 7.654, 9.986};
     for (int u = 0; u<weightsFractional.length-1; u++) {
         System.out.println(weightsFractional[u]+" ,");
     }
     System.out.println(weightsFractional[weightsFractional.length-1]);
+System.out.println();
+//3
+        int[] weightsMassive = new int[] {1, 2, 3};
+        for (int i = weightsMassive.length -1; i >=0; i--){
+            System.out.print(weightsMassive[i] + ",");}
+        System.out.print(weightsMassive[0]);
+        }
 
-
-        weights[0] = 12;
-        int januaryWeights = weights[0];
-        System.out.println(januaryWeights);
-        System.out.println(weights[0]);
-        System.out.println(weights[3]);
-        int january = 0;
-        System.out.println(weights[january]);
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
 
         }
-            
 
-        }
-    }
 
