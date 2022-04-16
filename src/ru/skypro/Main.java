921d6dc3c8b1d52e6ed6aa5f843d84f721cc54b5
ArtemCH240322 <sun2383@mail.ru>
2022-04-16 11:48:43 +0300

    Первый комит. Теория. Задачи //1,2,3

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,6 +1,5 @@
 package ru.skypro;
 
-import java.util.Arrays;
 
 public class Main {
 
@@ -10,51 +9,55 @@ public class Main {
         weight[1] = 2;
         weight[2] = 3;
         double[] weight2 = {1.57, 7.654, 9.986};
-        int[] weight3 = {1, 2, 3};
 
-        int[] weights = new int[]{0, 1, 2, 3};
-        for (int i = 0; i < weights.length - 1; i++) {
-            System.out.println(weights[i] + " ,");
+        for (int i = 0; i < weight.length; i++) {
+            if (i != weight.length - 1)
+                System.out.print(weight[i] + ", ");
+            else
+                System.out.print(weight[i]);
         }
-        System.out.print(weights[weights.length - 1]);
         System.out.println();
+
 //2
-        double[] weightsFractional = new double[]{1.57, 7.654, 9.986};
-        for (int u = 0; u < weightsFractional.length - 1; u++) {
-            System.out.print(weightsFractional[u] + " ,");
+
+        for (int u = 0; u < weight2.length; u++) {
+            if (u != weight2.length - 1)
+                System.out.print(weight2[u] + ", ");
+            else
+                System.out.print(weight2[u]);
         }
-        System.out.print(weightsFractional[weightsFractional.length - 1]);
         System.out.println();
 //3
-        int[] weightsMassive = new int[]{0, 1, 2, 3};
-        for (int i = weightsMassive.length - 1; i >= 0; i--) {
-            System.out.print(weightsMassive[i] + ",");
-        }
-        System.out.print(weightsMassive[0]);
 
+        for (int i = weight.length - 1; i >= 0; i--) {
+            if (i != 0)
+                System.out.print(weight[i] + ", ");
+            else
+                System.out.print(weight[i] + ", ");
+        }
         System.out.println();
         //
-        double[] weightsFractionalBack = new double[]{1.57, 7.654, 9.986};
-        for (int i = weightsFractionalBack.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(weightsFractionalBack[i]);
-            } else {
-                System.out.print(weightsFractionalBack[i] + ", ");
-            }
+        for (int i = weight2.length - 1; i >= 0; i--) {
+            if (i != 0)
+                System.out.print(weight2[i] + ", ");
+            else
+                System.out.print(weight2[i]);
         }
+        System.out.println();
 
         // 4
-        for (int i = 0; i <weights.length; i++) {
-            if (weight[i]%2==1){
-                weight[i] ++;
-            }
-            else {
-                System.out.println(weight[i] + ",");
-                System.out.println(0);
-                System.out.println(Arrays.toString(weight));
-            }
+        for (int i = 0; i < weight.length; i++) {
+            if (weight[i] % 2 == 1)
+                weight[i]++;
+
+            if (i != weight.length - 1)
+                System.out.print(weight[i] + ", ");
+            else
+                System.out.print(weight[i]);
         }
     }
 }
+
+
 
 
